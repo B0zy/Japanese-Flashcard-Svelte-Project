e4b0e9844647925d1151d13c05e09844c00f4616
Bozy <40073828+B0zy@users.noreply.github.com>
2024-07-21 22:56:03 -0400

    Change button

--- src/lib/Components/Button.svelte
+++ src/lib/Components/Button.svelte
@@ -1,9 +1,9 @@
 <script>
-	let { onclick } = $props();
+	let { text, onclick } = $props();
 </script>
 
 <div class="flex flex-col p-4 items-center">
-	<button class="bg-slate-800 rounded-2xl w-44 text-sky-50 text-xl p-4" {onclick}
-		>Check Answer</button
+	<button class="bg-slate-700 rounded-2xl w-44 text-sky-50 text-xl p-4 shadow" {onclick}
+		>{text}</button
 	>
 </div>
